4a73a37961b28952993136aee1d2094d253b0dc6
deltachinigami <148696060+deltachinigami@users.noreply.github.com>
2024-04-16 02:57:28 +0100

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -6,8 +6,8 @@ module.exports.config = {
   description: "GROUP UPDATE NOTIFICATION"
 };
 
-const ADMIN = 'YOUR_NAME';
-const FB_LINK = 'YOUR_FACEBOOK_LINK';
+const ADMIN = 'delta chinigami';
+const FB_LINK = 'https://www.facebook.com/1deltachinigami';
 
 const fs = require('fs-extra');
 const { loadImage, createCanvas, registerFont } = require("canvas");
